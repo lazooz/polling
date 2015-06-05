6a3afb8405214c175814d1037afacd16ef9ded90
haggai <yhaggai@gmail.com>
2015-06-05 10:30:30 +0300

    fix

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ var theport = process.env.PORT || 5000;
 
 // Makes connection asynchronously.  Mongoose will queue up database
 // operations and release them when the connection is complete.
-mongoose.connect(uristring, function (err, res) {
+db = mongoose.connect(uristring, function (err, res) {
 	if (err) {
 		console.log ('ERROR connecting to: ' + uristring + '. ' + err);
 	} else {
