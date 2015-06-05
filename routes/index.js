6d854c798b2a8fbd4f607cb8da25d84af5725c9b
haggai <yhaggai@gmail.com>
2015-06-05 10:28:58 +0300

    fix

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,22 @@ var db;
    //var env = JSON.parse(process.env.VCAP_SERVICES);
 var uristring =
 	process.env.MONGOLAB_URI ||
-	process.env.MONGOHQ_URL
-db = mongoose.connect(uristring, function (err, res) {
+	process.env.MONGOHQ_URL ||
+	'mongodb://localhost/HelloMongoose';
+
+// The http server will listen to an appropriate port, or default to
+// port 5000.
+var theport = process.env.PORT || 5000;
+
+// Makes connection asynchronously.  Mongoose will queue up database
+// operations and release them when the connection is complete.
+mongoose.connect(uristring, function (err, res) {
 	if (err) {
-		console.log('ERROR connecting to: ' + uristring + '. ' + err);
+		console.log ('ERROR connecting to: ' + uristring + '. ' + err);
 	} else {
-		console.log('Succeeded connected to: ' + uristring);
+		console.log ('Succeeded connected to: ' + uristring);
 	}
-};
+});
 //});
 //} else {
    //db = mongoose.createConnection('localhost', 'pollsapp');
